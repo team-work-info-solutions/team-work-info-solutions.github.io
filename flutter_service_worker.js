100ff1f4708926c0d2a97335bb1660408f6e125c
Nilesh Kumar Singh <nikkiba3kumari@gmail.com>
2022-11-02 17:05:11 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "cdc25d1a44627f81778ba46f311ec73d",
+  "assets/AssetManifest.json": "6b74de4f7654306ab4531429e617ca06",
 "assets/assets/checkmailbox.png": "5a437b1f5ffc50ba017ca7b412c9752a",
 "assets/assets/icons_image/cards.png": "0f3eccaa9f7f60b7fb41b0fa90cd7a28",
 "assets/assets/icons_image/delivery_man.png": "5ab796251cdb9ca68d694a7a5e63a969",
@@ -15,10 +15,9 @@ const RESOURCES = {
 "assets/assets/Trans7.png": "e97030537097007de9d7e18ef51aec1a",
 "assets/assets/translogo.png": "86113194bc9ba524e912a6d1efe218c3",
 "assets/assets/Transnational2.png": "d292a41872062b1dcaafedbf0b718413",
-"assets/FontManifest.json": "90fa59e9766da326ec32628962a23ec7",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/fonts/PublicSans-Regular.ttf": "d2d3b740e46522981f945573253b0a1a",
-"assets/NOTICES": "0fe700d9e3327158523c2c9ef15ce85e",
+"assets/NOTICES": "720ec78eee1e90fe606cbeb3facc69fe",
 "assets/packages/awesome_dialog/assets/flare/error.flr": "e3b124665e57682dab45f4ee8a16b3c9",
 "assets/packages/awesome_dialog/assets/flare/info.flr": "bc654ba9a96055d7309f0922746fe7a7",
 "assets/packages/awesome_dialog/assets/flare/info2.flr": "21af33cb65751b76639d98e106835cfb",
@@ -41,9 +40,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f0d05162dec8c21316ae086ac6acd588",
-"/": "f0d05162dec8c21316ae086ac6acd588",
-"main.dart.js": "c19c1bf8975e181b11871b1f1c34ce5d",
+"index.html": "56c2f08646cf1090e599d2db1621f60b",
+"/": "56c2f08646cf1090e599d2db1621f60b",
+"main.dart.js": "29ac0a07e196cbc8441d6db38cf12fea",
 "manifest.json": "d7f30a38893c436ff6e8f3dd8a89b807",
 "version.json": "3c4f83b362418867c33c5c09d989e2a1"
 };
